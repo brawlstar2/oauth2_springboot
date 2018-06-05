abbb693429b157796a5d1768ff605fe3014d8bf5
ROOT\Rahul <rhlsinha9@gmail.com>
2018-06-04 18:56:46 -0700

    first push

---
+++ src/main/java/com/sf/practice/oauth_springboot_prac/App.java
@@ -0,0 +1,13 @@
+package com.sf.practice.oauth_springboot_prac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
